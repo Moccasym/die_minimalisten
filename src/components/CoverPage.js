a7a93c662876dc65e31a697f7e3f1700113bfd7f
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-10-28 11:49:15 +0200

    added new designs/effects

--- src/components/CoverPage.js
+++ src/components/CoverPage.js
@@ -6,9 +6,11 @@ const CoverPage = () => {
   return (
     <div>
       <div>
+          <hr className="lineBreak"></hr>
           <h1>Die Minimalisten</h1>
+          <hr className="lineBreak"></hr>
           <p>Wir machen euch zu Minimalisten, damit ihr mehr Zeit und Geld habt</p>
-          <button className="button" href="/MinimalistGuide">Los Geht's</button>
+          <button className="button" href="/MinimalistGuide"><span>Los Geht's</span></button>
       </div>
     </div>
   );
